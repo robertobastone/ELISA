fe187cabc651306b76740f1868e31ad1c7728507
robertobastone <robertobastone93@gmail.com>
2023-08-29 19:24:55 +0200

    version 1.02

--- elisa.py
+++ elisa.py
@@ -7,10 +7,10 @@ import os # info about file
 import sys # better management of the exceptions
 import pandas as pd # open excel file
 from termcolor import colored # customize ui
-import elisa_plotSettings as plotsettings
+import elisa_settings as esettings
 
 ################################################### CONSTANTS
-numversion = "1.01"
+numversion = "1.02"
 textColor = "blue"
 welcomeText = "ELISA VERSION " + numversion + " IS PUTTING ON HER GLASSES"
 greetingText = "ELISA VERSION " + numversion + " HAS COMPLETED THE ANALYSIS"
@@ -21,6 +21,7 @@ dataLocation ='constellations.xlsx' # file name
 
 class main:
 
+    ########## start method
     def __init__(self):
         print(colored("--------------------------------------------------------------------------------------",textColor))
         print(colored("--------------------------------------------------------------------------------------",textColor))
@@ -28,15 +29,15 @@ class main:
         print(colored("--------------------------------------------------------------------------------------",textColor))
         print(colored("--------------------------------------------------------------------------------------",textColor))
         try:
-            ### generating mocha_plottersettings object with plot settings
-            settings = plotsettings.getPlotSetting()
-            self.survivalAnalysis(settings)
-            self.sayGreetings()
+            settings = esettings.getSettings() # loading plot settings
+            self.survivalAnalysis(settings) # Kaplan-Meier Analysis
+            self.sayGreetings() # ending quote
         except Exception as e:
             exc_type, exc_obj, exc_tb = sys.exc_info()
             print(colored(str(e), 'red'))
             print(colored(str(exc_tb.tb_frame.f_code.co_filename) + " at  line " + str(exc_tb.tb_lineno), 'red'))
 
+    ########## ending method
     def survivalAnalysis(self,settings):
         try:
             print(colored(workingText, textColor))
@@ -48,15 +49,17 @@ class main:
             E = df['E']
             groups = df['group']
             ix = (groups == 'miR-137')
+            upperX = self.calculateXUpperLimit(df['T'].max(),settings.xbase)
 
             controlGroup = KaplanMeierFitter().fit(T[~ix], E[~ix], label='control')
             ax = controlGroup.plot(ci_show=False)
             group1 = KaplanMeierFitter().fit(T[ix], E[ix], label='miR-137')
+            #print(T[ix])
             ax = group1.plot_survival_function(ax=ax, ci_show=False)
             
             ax.set_title(settings.title,fontsize=settings.titleFontSize)
             ########## x axis settings
-            ax.set_xlim(settings.xlim[0], df['T'].max()*1.1)
+            ax.set_xlim(settings.xlim[0],upperX)
             ax.set_xlabel(settings.xlabel,fontsize=settings.axisLabelFontSize)
             ########## y axis settings
             ax.set_ylim(settings.ylim[0],settings.ylim[1])
@@ -68,11 +71,42 @@ class main:
             fig.tight_layout()
             
             plt.savefig(settings.plotName, dpi=settings.dpi)
+
+            group2kpfit = {
+                "control": controlGroup,
+                "miR-137": group1,                
+            }
+
+            self.generateExcelFile(settings.excelFile,np.linspace(0.0, upperX, settings.tableRowsNumber),group2kpfit)
+
         except Exception as e:
             exc_type, exc_obj, exc_tb = sys.exc_info()
             print(colored(str(e), 'red'))
             print(colored(str(exc_tb.tb_frame.f_code.co_filename) + " at  line " + str(exc_tb.tb_lineno), 'red'))
 
+    ########## custom ceiling method
+    def calculateXUpperLimit(self, maxValue, base):
+        offset = 0
+        if maxValue % base == 0:
+            offset = base
+        return (-(maxValue // -base)*base)+offset
+
+    ########## save results in an excel file
+    def generateExcelFile(self, nameFile, timeline, group2kpfit):
+        try:
+            with pd.ExcelWriter(nameFile) as writer:  
+                for group in group2kpfit:
+                    survivalFunction = group2kpfit[group].survival_function_at_times(timeline) # get survival function (panda series)
+                    survivaltable =  survivalFunction.sort_values(0,ascending=False).reset_index().rename(columns={'index':'Time',group:'Survival Probability'}) # get results in table format
+                    # print(survivaltable) 
+                    # print(colored("--------------------------------------------------------------------------------------",textColor))           
+                    survivaltable.to_excel(writer, sheet_name=group, index=False, header=[settings.timeColumnName,self.survivalColumnName])
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            print(colored(str(e), 'red'))
+            print(colored(str(exc_tb.tb_frame.f_code.co_filename) + " at  line " + str(exc_tb.tb_lineno), 'red'))
+
+    ########## ending method
     def sayGreetings(self):
         print(colored("--------------------------------------------------------------------------------------",textColor))
         print(colored("--------------------------------------------------------------------------------------",textColor))
--- elisa_settings.py
+++ elisa_settings.py
@@ -2,13 +2,15 @@
 
 ######################### CODE #########################
 
-class getPlotSetting:
+class getSettings:
 
     def __init__(self):
+
+        ######################### PLOT SETTINGS
         ########## plot size
         self.figsize_x = 13
         self.figsize_y = 10
-        ########## plot size
+        ########## plot title
         self.title = 'Survival Function (Kaplan-Meier estimator)' 
         ########## plot font size
         self.titleFontSize = 20
@@ -17,13 +19,23 @@ class getPlotSetting:
         ########## x axis settings
         self.xlim = [0,1.1]
         self.xlabel = 'Time'
+        self.xbase = 10
+        ########## x axis settings
+        self.ylim = [0,1.05]
+        self.ylabel = 'Population Survival (percentage)'
         self.yticks = [0.2,0.4,0.6,0.8,1.0]
         self.yticksLabel = [20,40,60,80,100]
-        ########## x axis settings
-        self.ylim = [0,1.1]
-        self.ylabel = 'Population Survival (percentage)'
         ########## save settings
         self.dpi = 150
         self.plotName = 'elisa.png'
+
+        ######################### TABLE SETTINGS
+        ########## table settings
+        self.tableRowsNumber = 31
+        ########## table settings
+        self.timeColumnName = 'Time'
+        self.survivalColumnName = 'Survival Probability (percentage)'
+        ########## save settings
+        self.excelFile = 'results.xlsx'
         
         
